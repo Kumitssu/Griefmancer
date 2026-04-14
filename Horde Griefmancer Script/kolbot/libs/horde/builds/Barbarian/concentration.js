9446a877c5b261b48a7abfe27fa0773de80c1243
Kumitssu <gosckumer@gmail.com>
2026-04-14 15:09:00 +0200

    conc

---
+++ Horde Griefmancer Script/kolbot/libs/horde/builds/Barbarian/concentration.js
@@ -0,0 +1,17 @@
+/**
+*	@filename	concentration.js
+*	@author		Kumitssu
+*	@desc		Conc, bash, find item, bo, berserk - AIO barb
+*	@credits	Kumitssu, Adpist
+*
+*/
+
+var HordeBuild = {
+	statsBuild: "LevellingDex",
+	skillsBuild: "Concentration",
+	autoBuildTemplate: "Concentration",
+	mercAct2Normal: "Prayer",
+	mercAct2Nightmare: "Blessed Aim",
+	xpacPickits: ["horde/common.physical.xpac.nip", "horde/common.physical.shield.xpac.nip", "horde/barbarian.concentration.xpac.nip", "horde/merc.act2.meditation.xpac.nip"],
+	classicPickits: ["horde/common.caster.classic.nip", "horde/barbarian.shouting.classic.nip"] ///don't use it, doesn't work on classic
+};
